03020ea6a5f4edd14129e5aac413c1dc78c87daf
CTZDev <carlosteranzavaleta@hotmail.com>
2024-07-26 18:29:30 -0500

    refactor: Replace import styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ShoppingCartApp from './ShoppingCartApp';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
