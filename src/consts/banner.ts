87e022f9437f9fdd2e9a6940a67645402a57d531
CTZDev <carlosteranzavaleta@hotmail.com>
2024-07-29 22:11:53 -0500

    Add data for components

---
+++ src/consts/banner.ts
@@ -0,0 +1,22 @@
+import type { BannerItem } from '../types/Banner';
+
+export const BANNER: BannerItem[] = [
+  {
+    url: './icono1.png',
+    title: 'Focus',
+    firstContent: '20,000 Cursos en Línea',
+    secondContent: 'Explora los temas más recientes',
+  },
+  {
+    url: './icono2.png',
+    title: 'Target',
+    firstContent: 'Instructores Expertos',
+    secondContent: 'Aprende con distintos estilos',
+  },
+  {
+    url: './icono3.png',
+    title: 'Timer',
+    firstContent: 'Acceso de por vida',
+    secondContent: 'Aprende a tu ritmo',
+  },
+];
---
+++ src/consts/links.ts
@@ -0,0 +1,36 @@
+import { Link } from '../types/Link';
+
+export const LINKS: Array<Link> = [
+  {
+    url: '/',
+    title: 'Para tu negocio',
+  },
+  {
+    url: '/',
+    title: 'Conviértete en instructor',
+  },
+  {
+    url: '/',
+    title: 'Aplicaciones móviles',
+  },
+  {
+    url: '/',
+    title: 'Soporte',
+  },
+  {
+    url: '/',
+    title: 'Temas',
+  },
+  {
+    url: '/',
+    title: '¿Quienes somos?',
+  },
+  {
+    url: '/',
+    title: 'Empleo',
+  },
+  {
+    url: '/',
+    title: 'Blog',
+  },
+];
