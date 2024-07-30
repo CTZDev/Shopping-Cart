a2a6f5140b404bf999635bfe65ea0dba323010b6
CTZDev <carlosteranzavaleta@hotmail.com>
2024-07-29 22:11:09 -0500

    Layout finished of the App

--- src/ShoppingCartApp.tsx
+++ src/ShoppingCartApp.tsx
@@ -1,15 +1,19 @@
-import Banner from './components/Banner';
-import Courses from './components/Courses';
-import Header from './components/Header';
-import Hero from './components/Hero';
+import Banner from './components/Banner/Banner';
+import Courses from './components/Courses/Courses';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <Banner />
+      <main>
+        <Hero />
+        <Banner />
+      </main>
       <Courses />
+      <Footer />
     </>
   );
 }
