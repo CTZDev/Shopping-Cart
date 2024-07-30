374722f9f1747c269925dac9f9579ca7f6e58fdf
CTZDev <carlosteranzavaleta@hotmail.com>
2024-07-29 22:13:39 -0500

    Add Button for Course

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+const Button: React.FC = () => {
+  return (
+    <>
+      <button type='button' className='btn btn-primary'>
+        Agregar al carrito
+      </button>
+    </>
+  );
+};
+
+export default Button;
