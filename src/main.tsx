ba565282b65acf4d49e102e54ecffbd34dc33a39
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-08 19:19:30 -0500

    feat: Add provider CoursesProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import ShoppingCartApp from './ShoppingCartApp';
 import '../index.css';
 import { CartProvider } from './contexts/CartContext';
+import { CoursesProvider } from './contexts/CoursesContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CartProvider>
-      <ShoppingCartApp />
+      <CoursesProvider>
+        <ShoppingCartApp />
+      </CoursesProvider>
     </CartProvider>
   </React.StrictMode>
 );
